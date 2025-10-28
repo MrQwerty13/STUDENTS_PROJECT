2462085263595d58688329c63886baaea36849ff
Mikhail P <pozur07@icloud.com>
2025-10-29 00:09:47 +0300

    MrQwerty13 sudjested a lib for web-app

---
+++ streamlit.py
@@ -0,0 +1,13 @@
+# Small Python script for our project for web-application
+# To run this file, use the command: streamlit run file_py1.py
+
+import streamlit as s
+
+s.color_picker("Choose a colour")
+
+s.write("""
+        # Добро пожаловать в наше веб-приложение написанное на Streamlit!
+        """)
+s.write("""
+        Он умеет все, что нужно нам, для создания красивого frontend
+        """)
