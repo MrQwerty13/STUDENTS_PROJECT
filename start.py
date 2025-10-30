38e0627d36c43758bf1f032c0283b46a505ebc40
Mikhail P <pozur07@icloud.com>
2025-10-30 16:25:46 +0300

    Added file strat.py for possible start

---
+++ start.py
@@ -0,0 +1,14 @@
+class Block:
+
+    def __init__(self, coding, way):
+        self.coding = coding
+        self.way = way
+
+    def to_code(_):
+        ...
+    
+    def the_way(_):
+        ...
+
+    def dividing(_):
+        ...
