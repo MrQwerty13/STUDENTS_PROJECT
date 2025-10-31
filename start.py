f285d4532b87f06e14d1a20893de83e09ce265fa
Mikhail P <pozur07@icloud.com>
2025-10-31 13:59:23 +0300

    Deleted code for new version

--- start.py
+++ start.py
@@ -1,14 +1,1 @@
-class Block:
-
-    def __init__(self, coding, way):
-        self.coding = coding
-        self.way = way
-
-    def to_code(_):
-        ...
-    
-    def the_way(_):
-        ...
-
-    def dividing(_):
-        ...
+# Importing necessary modules for blockchain interaction
